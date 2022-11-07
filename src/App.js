2bc3912d897dc298a50fff049fba6eced0adc64c
Oliver Fries <oliv481u@videndjurs.net>
2022-11-07 17:07:34 +0100

    Layout mostly finished.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.scss';
 import Header from './components/layout/Header';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      layout
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<Navbar />
+			<main>
+				{/* Routes here */}
+				<Forside />
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
+
+
+const Forside = () => {
+	return <div style={{ height: "600px", ["background-color"]: "grey" }}>
+
+	</div>
+}
