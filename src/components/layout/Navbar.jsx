ecf754446554e3967bb0a71b0c011a15c480ff7c
DrDankinson <uglemave@gmail.com>
2022-11-11 22:06:08 +0100

    Burger menu finished.

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import styles from "./styles/Navbar.module.scss"
 
 const Navbar = () => {
 
     const [scroll, setScroll] = useState()
+    const [mobileNavOpen, setMobileNavOpen] = useState(false)
     const navRef = useRef()
 
     useEffect(() => {
@@ -13,21 +14,41 @@ const Navbar = () => {
         return () => document.removeEventListener('scroll', () => setScroll(window.scrollY))
     }, [])
 
-    return <nav ref={navRef} className={`${styles["layout-navbar"]} ${navRef.current && scroll > navRef.current.offsetHeight + 80 ? styles["sticky"] : ""}`}>
-        <ul>
-            <li><NavbarLink to="/">FORSIDE</NavbarLink></li>
-            <li><NavbarLink to="/om-os">OM OS</NavbarLink></li>
-            <li><NavbarLink to="/services">SERVICE</NavbarLink></li>
-            <li><NavbarLink to="/faq">FAQ</NavbarLink></li>
-            <li><NavbarLink to="/nyheder">NYHEDER</NavbarLink></li>
-            <li><NavbarLink to="/kontakt">KONTAKT OS</NavbarLink></li>
-        </ul>
+    return <>
+        <nav ref={navRef} className={`${styles["layout-navbar"]} ${navRef.current && scroll > navRef.current.offsetHeight + 80 ? styles["sticky"] : ""}`}>
 
-        <div className={styles["layout-navbar-search-container"]}>
-            <input type="text" placeholder="Søg" />
-            <button className="glyphicons-search"></button>
-        </div>
-    </nav>
+            <ul className={styles["layout-navbar-pc"]}>
+                <li><NavbarLink to="/">FORSIDE</NavbarLink></li>
+                <li><NavbarLink to="/om-os">OM OS</NavbarLink></li>
+                <li><NavbarLink to="/services">SERVICE</NavbarLink></li>
+                <li><NavbarLink to="/faq">FAQ</NavbarLink></li>
+                <li><NavbarLink to="/nyheder">NYHEDER</NavbarLink></li>
+                <li><NavbarLink to="/kontakt">KONTAKT OS</NavbarLink></li>
+            </ul>
+
+            <div className={styles["layout-navbar-mobile"]} onClick={() => setMobileNavOpen(true)}>
+                <img src="/images/icon/submenu-icon.png" alt="burger menu" />
+            </div>
+
+            <div className={styles["layout-navbar-search-container"]}>
+                <input type="text" placeholder="Søg" />
+                <button className="glyphicons-search"></button>
+            </div>
+        </nav>
+
+        <ul className={`${styles["layout-mobile-navigation"]} ${mobileNavOpen ? styles.active : ""}`}>
+            <button aria-label="return" onClick={() => setMobileNavOpen(false)}>
+                <img src="/images/icon/icon-cross.png" alt="cross" />
+            </button>
+
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/">FORSIDE</NavbarLink></li>
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/om-os">OM OS</NavbarLink></li>
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/services">SERVICE</NavbarLink></li>
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/faq">FAQ</NavbarLink></li>
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/nyheder">NYHEDER</NavbarLink></li>
+            <li onClick={() => setMobileNavOpen(false)}><NavbarLink to="/kontakt">KONTAKT OS</NavbarLink></li>
+        </ul>
+    </>
 }
 
 const NavbarLink = ({ to, children }) =>
